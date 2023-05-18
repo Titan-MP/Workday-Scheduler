862b2924bdd7d2f3ba7085c6348b18df459f3d4e
Titan-MP <marcrhymaun@knights.ucf.edu>
2023-05-18 00:51:31 -0400

    almost done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,53 @@
-let time = new Date();
-currentDay.innerHTML = time.toDateString();
-const main = document.querySelector('main');
-const hours = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM'];
+let time = dayjs();
+let store;
 
-hours.forEach((hour,i) => {
-  let rH = i+9;
-  let cH = time.getHours();
+let getStore = async () => {
+  store = (await localStorage.hours) ? JSON.parse(localStorage.hours) : [];
+  store.forEach((task, i) => {
+    $("textarea").eq(i).val(task);
+  });
+};
+
+getStore();
+
+currentDay.innerHTML = time.format("MMM D YYYY, h:mm A ");
+
+let hours = [
+  "9AM",
+  "10AM",
+  "11AM",
+  "12PM",
+  "1PM",
+  "2PM",
+  "3PM",
+  "4PM",
+  "5PM",
+];
+
+hours.forEach((hour, i) => {
+  let cH = time.format("H");
+  let rH = i + 1;
 
   main.innerHTML += `
-  <div id="hour-9" class="row time-block ${rH<cH ? 'past' : rH>cH ? 'future' : 'present'} ">
-    <div class="col-2 col-md-1 hour text-center py-3">${hour}</div>
-    <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
-    <button class="btn saveBtn col-2 col-md-1" aria-label="save">
-      <i class="fas fa-save" aria-hidden="true"></i>
-    </button>
-</div>
+    <div id="hour-${rH}" class="row time-block ${
+      rH < cH ? "past" : rH > cH ? "future" : "present"
+    }">
+      <div class="col-2 col-md-1 hour text-center py-3">${hour}</div>
+      <textarea class="col-8 col-md-10 description" rows="3"></textarea>
+      <button onclick="saveStore()" class="btn saveBtn col-2 col-md-1" aria-label="save">
+        <i class="fas fa-save" aria-hidden="true"></i>
+      </button>
+    </div>
   `;
 });
+
+const saveStore = () => {
+  const areas = document.querySelectorAll("textarea");
+  let store = [];
+
+  areas.forEach((area) => {
+    store.push(area.value);
+  });
+
+  localStorage.hours = JSON.stringify(store);
+};
